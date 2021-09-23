0b63e12edbedd5e362bf7d10cec74fec48046665
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-09-23 10:57:27 +0530

    added chapter 7

---
+++ chapter6.py
@@ -0,0 +1,15 @@
+#------ Joining Images ------#
+
+import cv2
+import numpy as np
+
+img = cv2.imread('resources/xyber.jpg')
+img_resize = cv2.resize(img, (370, 480))
+
+# The images should have same no of color channels
+imgHor = np.hstack((img_resize, img_resize, img_resize))
+imgVer = np.vstack((img_resize, img_resize))
+
+cv2.imshow("Horizontal", imgHor)
+cv2.imshow("Vertical", imgVer)
+cv2.waitKey(0)
