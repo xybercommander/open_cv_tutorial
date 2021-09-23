ca16efc5ab907c3a3c009831d6b09a5d5ffe1c4e
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-09-23 10:58:06 +0530

    added chapter 6

--- chapter6.py
+++ chapter6.py
@@ -6,6 +6,8 @@ import numpy as np
 img = cv2.imread('resources/xyber.jpg')
 img_resize = cv2.resize(img, (370, 480))
 
+#TODO ---> SCALE IMAGE
+
 # The images should have same no of color channels
 imgHor = np.hstack((img_resize, img_resize, img_resize))
 imgVer = np.vstack((img_resize, img_resize))
